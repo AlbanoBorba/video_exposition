dccb71338287e01cf83d14e75c2c1cd1f961b349
Albano <albano.b06@gmail.com>
2019-07-01 21:43:26 -0300

    SAVEPOINT

--- data_utils/video_map.py
+++ data_utils/video_map.py
@@ -45,8 +45,8 @@ if __name__ == '__main__':
 			if f.endswith('.mov') and f.startswith('n_'):
 				read_video(os.path.join(root,f))
 
-	print('Count:' count)
-	print('Sum:', avg)
+	print('Count: ', count)
+	print('Sum: ', avg)
 	print('Mean: ', avg.mean())
 	print('Std: ', avg.std())
 
