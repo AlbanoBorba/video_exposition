cf27835b2f1bc6b60ef48cddf20fa1e969281641
Albano <albano.b06@gmail.com>
2019-07-02 16:31:54 -0300

    SAVEPOINT

--- train.py
+++ train.py
@@ -79,6 +79,7 @@ def train_model(model, dataloader, criterion, optimizer, num_epochs=25, phases=[
                             print(datetime.datetime.now())
 
                     # statisti
+                    print(loss.item())
                     running_loss += loss.data[0]
                     print('loss.item()')
                     print('\t', end='')
