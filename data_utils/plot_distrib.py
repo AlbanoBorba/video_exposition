abfb749e37b758e6ee354373267be6b9b92b2a33
Albano <albano.b06@gmail.com>
2019-07-04 19:54:27 -0300

    SAVEPOINT

--- data_utils/plot_distrib.py
+++ data_utils/plot_distrib.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 
-f = open('out.txt', "r")
+f = open('night_distrib.txt', "r")
 means = f.read().replace(', ','\n').split('\n')
 n = means.pop()
 n = means.pop()
 
-plt.plot([float(x) for x in means], [x for x in range(int(n))], 'ro')
+means = [float(x) for x in means]
+
+plt.hist(means, color = 'blue', edgecolor = 'black', bins = 50)
+plt.title('Luminanica dos Vídeos Noturnos')
+plt.xlabel('Intensidade média dos pixels')
+plt.ylabel('Número de vídeos')
+
 plt.show()
