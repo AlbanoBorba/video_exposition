68e250fe5932e2326dec88d1a3148b7e88b4b8c8
Albano <albano.b06@gmail.com>
2019-07-08 20:01:26 -0300

    SAVEPOINT

--- data_utils/video_map.py
+++ data_utils/video_map.py
@@ -38,7 +38,7 @@ def read_image(path):
 
 avg = []
 count = 0
-file = open("day_distrib.txt", "w")
+file = open("all_distrib.txt", "w")
 
 if __name__ == '__main__':	
 	path = '/media/albano/external'
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 
 	for root, dirs, files in os.walk(path):
 		for f in files:
-			if f.endswith('.mov') and f.startswith('d_'):
+			if f.endswith('.mov'):
 				read_video(os.path.join(root,f))
 
 	#r = [x for x in range(count)]
