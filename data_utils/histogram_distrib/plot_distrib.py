c1f5e0b8bd5c06c9615b6eb852664b5b0fa22d60
Albano <albano.b06@gmail.com>
2019-07-12 17:12:43 -0300

    SAVEPOINT

--- data_utils/histogram_distrib/plot_distrib.py
+++ data_utils/histogram_distrib/plot_distrib.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 f = open('all_distrib.txt', "r")
 means = f.read().split('\n')
@@ -10,5 +11,6 @@ plt.hist(means, color = 'blue', edgecolor = 'black', bins = 50)
 plt.title('Luminanica de todos os vídeos')
 plt.xlabel('Intensidade média dos pixels')
 plt.ylabel('Número de vídeos')
+plt.xticks(np.arange(0, 255, 25))
 
 plt.show()
