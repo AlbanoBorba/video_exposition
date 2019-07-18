45a03b239cb0f085cf77291aec4977491640a7e1
Albano <albano.b06@gmail.com>
2019-07-18 19:38:33 -0300

    SAVEPOINT

--- data_utils/video_map.py
+++ data_utils/video_map.py
@@ -4,31 +4,30 @@ from skimage import io, transform
 import cv2
 import matplotlib.pyplot as plt
 
-def read_video(video_path):
+def read_video(video_path, frames=40):
 	cap = cv2.VideoCapture(video_path)
 
-	global avg
-	global count
 	global file
+	count = 0
+	x = []
 
 	while True:
 		ret, frame = cap.read()
 		
 		if ret:
 			
-			#print(count)
-			count = count + 1
-			x = np.average(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-			file.write(str(x)+'\n')
-			avg.append(x)
-			#avg = avg + frame.reshape((1280,720,3))
+			x.append(np.average(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)))
 	
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
-			break
+
+			if count == frames: break
+			else: count += 1
 		else:
 			break
 	
+	file.write(str(np.average(x))+'\t'+str(np.std(x))+'\n')
+
 	cap.release()
 	cv2.destroyAllWindows()
 
@@ -36,9 +35,7 @@ def read_image(path):
 	img = io.imread(path)
 	medians.append(np.median(img))
 
-avg = []
-count = 0
-file = open("all_distrib.txt", "w")
+file = open("all_distrib_40f.txt", "w")
 
 if __name__ == '__main__':	
 	path = '/media/albano/external'
@@ -48,11 +45,3 @@ if __name__ == '__main__':
 		for f in files:
 			if f.endswith('.mov'):
 				read_video(os.path.join(root,f))
-
-	#r = [x for x in range(count)]
-
-	print(avg)
-	print(count)
-
-
-
