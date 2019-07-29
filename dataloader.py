1f6f47042e54c7918102674b8b8a44ef9c50b092
Albano <albano.b06@gmail.com>
2019-07-29 15:51:21 -0300

    SAVEPOINT

--- dataloader.py
+++ dataloader.py
@@ -21,8 +21,8 @@ def transforms_list():
         transforms.CenterCrop((400, 400)),
         transforms.Lambda(lambda x: rotate(x)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0.279, 0.293, 0.290), std=(0.197, 0.198, 0.201))
-        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        #transforms.Normalize(mean=(0.279, 0.293, 0.290), std=(0.197, 0.198, 0.201))
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
 
 def custom_collate(batch):
