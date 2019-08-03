09c27d3802e7c707ca582046864b1753b4ec1e10
Albano <albano.b06@gmail.com>
2019-08-03 12:24:41 -0300

    (FIX) reduce vgg

--- loss_utils/vgg.py
+++ loss_utils/vgg.py
@@ -16,10 +16,10 @@ class Vgg16(torch.nn.Module):
             self.slice1.add_module(str(x), vgg_pretrained_features[x])
         for x in range(4, 9):
             self.slice2.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(9, 16):
-            self.slice3.add_module(str(x), vgg_pretrained_features[x])
-        for x in range(16, 23):
-            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        # for x in range(9, 16):
+            # self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        # for x in range(16, 23):
+            # self.slice4.add_module(str(x), vgg_pretrained_features[x])
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad = False
@@ -29,10 +29,10 @@ class Vgg16(torch.nn.Module):
         h_relu1_2 = h
         h = self.slice2(h)
         h_relu2_2 = h
-        h = self.slice3(h)
-        h_relu3_3 = h
-        h = self.slice4(h)
-        h_relu4_3 = h
-        vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2', 'relu3_3', 'relu4_3'])
-        out = vgg_outputs(h_relu1_2, h_relu2_2, h_relu3_3, h_relu4_3)
+        # h = self.slice3(h)
+        # h_relu3_3 = h
+        # h = self.slice4(h)
+        # h_relu4_3 = h
+        vgg_outputs = namedtuple("VggOutputs", ['relu1_2', 'relu2_2'])#, 'relu3_3', 'relu4_3'])
+        out = vgg_outputs(h_relu1_2, h_relu2_2)#, h_relu3_3, h_relu4_3)
         return out
