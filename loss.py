8f655bcf97cf4ff7ee386528993a04d18e60dc94
Albano <albano.b06@gmail.com>
2019-08-03 12:49:58 -0300

    SAVEPOINT

--- loss.py
+++ loss.py
@@ -3,14 +3,7 @@ import torch.nn as nn
 from loss_utils.vgg import Vgg16
 from utils import log
 from torchvision import transforms
-
-
-def transform_list():
-    return [
-        transforms.ToPILImage(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        transforms.ToTensor(),
-    ]
+import pytorch_colors as colors
 
 class LossFunction(nn.Module):
     def __init__(self, weight=1):
@@ -25,24 +18,18 @@ class LossFunction(nn.Module):
 
     def forward(self, x, y):
 
-        # print('*'*10)
-        # print(x.shape)
-        # print(y.shape)
-
+        # change colorspace
+        x = colors.rgb_to_yuv(x)
+        y = colors.rgb_to_yuv(y)
+        
+        # mse loss
         loss_mse = self.mse(x, y)
-        x_vgg = self.vgg(x)#self.transform(x))
-        y_vgg = self.vgg(y)#self.transform(y))
-
-        #log.log_images_vgg(x_vgg.relu2_2, y_vgg.relu2_2, './results/')
-
-        #print('Shape vgg:')
-        #print(x_vgg.relu2_2.shape)
-        #print(y_vgg.relu2_2.shape)
+        
+        # feature loss
+        x_vgg = self.vgg(x)
+        y_vgg = self.vgg(y)
         loss_vgg = self.mse_vgg(x_vgg.relu2_2, y_vgg.relu2_2)
-        #print('\nLoss vgg: ', loss_vgg)
-
+        
         loss = loss_mse + (0.1 * loss_vgg)  # ajustar
-        #print('\nLoss total: ', loss)
-        #print('\n')
-
+        
         return loss
