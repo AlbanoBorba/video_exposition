4b447c9a0fd3d3457b4802d0fa04e765f209ecd5
Albano <albano.b06@gmail.com>
2019-08-08 18:50:22 -0300

    SAVEPOINT

--- loss_utils/models/networks_basic.py
+++ loss_utils/models/networks_basic.py
@@ -12,7 +12,7 @@ from skimage import color
 from IPython import embed
 from . import pretrained_networks as pn
 
-from main import *
+from models.main import *
 
 def spatial_average(in_tens, keepdim=True):
     return in_tens.mean([2,3],keepdim=keepdim)
