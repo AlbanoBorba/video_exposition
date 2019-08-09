7b6a7db29acdd77ad6d70f96fa42b22e79ea8f3d
Albano <albano.b06@gmail.com>
2019-08-09 15:30:12 -0300

    SAVEPOINT

--- data_utils/csv_map.py
+++ data_utils/csv_map.py
@@ -10,14 +10,16 @@ import pandas as pd
 def read_video(path, n_frames=50):
     cap = cv2.VideoCapture(path)
     count = 0
-    video_avg = []
+
+	# cria um novo folder para o vídeo
 
     while True:
         ret, frame = cap.read()
 
         if ret:
-            frame_avg = np.average(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
-            video_avg.append(frame_avg)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        	# preprocessa o frame
+			# salva o frame
 
             count += 1
             if count == n_frames:
@@ -28,15 +30,7 @@ def read_video(path, n_frames=50):
         else:
             break
 
-    video_length = 100  # int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     cap.release()
-
-    return video_length, np.average(video_avg)
-
-
-def read_csv():
-    pass
-
 
 if __name__ == '__main__':
     csv_path = './csv_loaders/bdd_day[90-110]_all.csv'
@@ -44,10 +38,6 @@ if __name__ == '__main__':
     csv_file = pd.read_csv(csv_path)
 
     for index, row in csv_file.iterrows():
-        print(index, row)
+        read_video(row.values[0])
 
-		# abro um vídeo e pego n frames
-		# preprocessa o frame
-		# salvo os frames em pasta por vídeo no hd
-
-        
+        # salvo os frames em pasta por vídeo no hd
