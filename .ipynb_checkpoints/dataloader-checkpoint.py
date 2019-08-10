62322cd5825fe9889274601e24908338d7251136
Albano <albano.b06@gmail.com>
2019-08-10 19:25:34 -0300

    SAVEPOINT

---
+++ .ipynb_checkpoints/dataloader-checkpoint.py
@@ -0,0 +1,157 @@
+from __future__ import print_function, division
+import os
+import sys
+import numpy as np
+import pandas as pd
+import torch
+import random
+import cv2
+from skimage import color
+from scipy import ndimage, misc
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import imageio
+
+def rotate(frame):
+    return ndimage.rotate(frame, 90, reshape=True) 
+
+def to_yuv(frame):
+    return color.rgb2yuv(frame)
+
+def transforms_list():
+    return [
+        #transforms.ToPILImage(),
+        transforms.Resize((400, 720)),
+        transforms.CenterCrop((400, 400)),
+        transforms.Lambda(lambda x: rotate(x)),
+        #transforms.Lambda(lambda x: to_yuv(x)),
+        transforms.ToTensor(),
+        #transforms.Normalize(mean=(0.279, 0.293, 0.290), std=(0.197, 0.198, 0.201))
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]
+
+def custom_collate(batch):
+    data = torch.stack([item['x'] for item in batch], dim=0)
+    target = torch.stack([item['y'] for item in batch], dim=0)
+
+    return {'x':data, 'y':target}
+
+
+class BddDaloaderFactory():
+
+    def __init__(self, csv_path, exposure, batch_size, n_videos='total', n_samples=40, window_size=3):
+
+        if exposure == 'under':
+            self.gamma = [2, 4, 6]
+            #self.gamma = [4, 6, 8]
+        elif exposure == 'over':
+            self.gamma = [0.1, 0.2, 0.4]
+            #self.gamma = [1/4, 1/6, 1/8]
+            
+        else:
+            sys.exit("O tipo de exposiçao deve ser 'under' ou 'over'!")
+
+        self.batch_size = batch_size
+        self.n_samples = n_samples
+        self.window_size = window_size
+        self.video_loader = pd.read_csv(csv_path)
+
+        if (n_videos == 'total'): self.n_videos = len(self.video_loader.index)
+        else: self.n_videos = n_videos
+
+    #def __len__(self):
+    #    if self.n_videos == 'total':
+    #        return len(self.video_loader.index)
+    #    return self.n_videos
+
+    #def __getitem__(self, idx):
+    def iterate(self):
+        for i in range(self.n_videos):
+            random_video = self.video_loader.sample(n=1)
+            video_path = random_video['video_path'].tolist()[0] # str
+
+            dataset = SingleVideoDataset(video_path, self.n_samples, self.window_size, random.choice(self.gamma))
+
+            dataloader = DataLoader(dataset=dataset, 
+                                    batch_size=self.batch_size, 
+                                    num_workers=0,
+                                    collate_fn=custom_collate)
+
+            yield i, dataloader
+
+
+class SingleVideoDataset(Dataset):
+
+    def __init__(self, video_path, n_samples, window_size, gamma, transform=transforms.Compose(transforms_list())):
+
+        self.sample_loader = SampleLoader(video_path, window_size)
+        self.n_samples = n_samples
+        self.gamma = gamma
+        self.transform = transform
+
+    def __len__(self):
+        return self.n_samples
+    
+    # Enumerate call
+    def __getitem__(self, idx):
+
+        # Get window_size frames
+        frames = self.sample_loader.get_sample()
+
+        # Preprocess ground-truth
+        frame_gt = frames[int(len(frames)/2)]
+        #frame_gt = ndimage.rotate(frame_gt, 90, reshape=True)      
+        frame_gt = transforms.functional.to_pil_image(frame_gt)      
+        frame_gt = self.transform(frame_gt)
+        #utils.save_image(frame_gt, './results/teste/gt_{}.png'.format(idx))
+
+
+        # Preprocess window
+        window = []
+        #count = 0
+        for frame in frames:
+            frame = self.change_gamma(frame, self.gamma)
+            frame = self.transform(frame)
+            #utils.save_image(frame, './results/teste/item_{}_{}.png'.format(idx, count))
+            #count += 1
+
+            window.append(frame)
+
+        window = torch.stack(window, dim=1)
+
+        # Set sampleW
+        sample = {
+            'x': window,
+            'y': frame_gt
+        }
+
+        return sample
+
+    def change_gamma(self, f, gamma):
+        f = transforms.functional.to_pil_image(f)
+        f = transforms.functional.adjust_gamma(f, gamma)
+
+        return f
+
+class SampleLoader():
+
+    def __init__(self, video_path, window_size):
+        self.cap = cv2.VideoCapture(video_path)
+        self.window_size = window_size
+        self.index = 0
+        self.frames = []
+
+    
+    def get_sample(self):
+
+        if self.index == 0:
+            for i in range(self.window_size):
+                _, frame = self.cap.read()
+                self.frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            self.index = 1
+        else:
+            self.frames.pop(0)
+            _, frame = self.cap.read()
+            self.frames.append(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        return self.frames
--- data_utils/.ipynb_checkpoints/csv_map-checkpoint.py
+++ data_utils/.ipynb_checkpoints/csv_map-checkpoint.py
@@ -1,29 +1,37 @@
 import os
-import numpy as np
-from skimage import io, transform
 import cv2
-import matplotlib.pyplot as plt
 import csv
 import pandas as pd
+import numpy as np
+from skimage import io, transform
 
-
-def read_video(path, n_frames=50, out_path):
-    cap = cv2.VideoCapture(path)
-    count = 0
+def read_video(video_path, n_frames=50, out_path='/media/albano/bdd100k_images/'):
 
     # cria um novo folder para o vídeo
+    video_name = video_path.split('/')[-1].split('.')[-2]
+    out_path = out_path + video_name + '/'
+    os.mkdir(out_path)
+    
+    # abre o vídeo
+    cap = cv2.VideoCapture(video_path)
+    count = 0
 
     while True:
         ret, frame = cap.read()
 
         if ret:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # preprocessa o frame
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame = transform.resize(frame, (720, 405))
+            frame = transform.rotate(frame, 90, resize=True)
+            
             # salva o frame
-
-            count += 1
-            if count == n_frames:
+            io.imsave("{}{:02d}.png".format(out_path, count), frame)
+                        
+            if count == (n_frames - 1):
                 break
+            else:
+                count += 1
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
@@ -34,11 +42,10 @@ def read_video(path, n_frames=50, out_path):
 
 if __name__ == '__main__':
     csv_path = './csv_loaders/bdd_day[90-110]_all.csv'
-    out_path = ''
+    max_videos = 10000
     
-    csv_file = pd.read_csv(csv_path)
+    video_df = pd.read_csv(csv_path)
 
-    for index, row in csv_file.iterrows():
-        read_video(row.values[0])
-
-        # salvo os frames em pasta por vídeo no hd
+    for i in range(max_videos):
+        video_path = video_df.sample(n=1, replace=False)
+        read_video(video_path.values[0][0])
