b1154566c0fdbf0a2c0c4a08ce76b66317332bd0
Albano <albano.b06@gmail.com>
2019-08-14 22:01:20 -0300

    SAVEPOINT

---
+++ max_loss_v3.py
@@ -0,0 +1,9 @@
+def loss_mix_v3(y_true, y_pred):
+    alpha = 0.2
+    l1_w = 1-alpha
+    ms_ssim_w = alpha
+    
+    l1 = K.mean(K.abs(y_pred - y_true)*K.abs(y_true - .5), axis=-1)
+    ms_ssim = tf.reduce_mean(1-tf.image.ssim_multiscale(y_pred, y_true, max_val = 1.0))
+    
+    return ms_ssim_w*ms_ssim + l1_w*l1
